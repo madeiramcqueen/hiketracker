e0a331b4c9802acfd6376c82f026e397af3fbcac
madeiramcqueen <madeira@mcqueen.net>
2023-01-14 13:40:54 -0800

    Add hike data to hikes page, practice

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
-    path('hikes/', views.hikes, name='hikes'),
+    path('hikes/', views.hikes_index, name='index'),
 ]
 
--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+class Hike:
+    def __init__(self, name, location, length, elevation, level, description):
+        self.name = name
+        self.location = location
+        self.length = length
+        self.elevation = elevation
+        self.level = level
+        self.description = description
+        
+hikes = [
+    Hike('Mount Si', 'North Bend, WA', '8 miles', '3150 ft', 'Difficult', 'Very steep but rewarding!'),
+]
+
 # Home view
 def home(request):
     return HttpResponse('<h1> Welcome to  your Hike Tracker! </h1>')
@@ -10,5 +23,5 @@ def about(request):
     return render(request,'about.html')
 
 # My Hikes view
-def hikes(request):
-    return render(request, 'hikes.html')
+def hikes_index(request):
+    return render(request, 'hikes/index.html', {'hikes': hikes})
