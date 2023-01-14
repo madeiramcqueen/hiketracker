1e11466f942da75a6b239ff28b26055342d98182
madeiramcqueen <madeira@mcqueen.net>
2023-01-14 12:04:50 -0800

    Add hikes url, view, and template

--- main_app/views.py
+++ main_app/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-# Create your views here.
+# Home view
 def home(request):
     return HttpResponse('<h1> Welcome to  your Hike Tracker! </h1>')
 
+# About view
 def about(request):
     return render(request,'about.html')
+
+# My Hikes view
+def hikes(request):
+    return render(request, 'hikes.html')
