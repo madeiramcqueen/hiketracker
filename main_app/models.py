e186b148d18befbfdb45b084257bcefd939f09c7
madeiramcqueen <madeira@mcqueen.net>
2023-01-18 15:52:45 -0800

    Add Hike Model and add migrations

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Hike(models.Model):
+    name = models.CharField(max_length=100)
+    location = models.CharField(max_length=100)
+    length = models.IntegerField()
+    elevation = models.IntegerField()
+    description = models.TextField(max_length=300)
