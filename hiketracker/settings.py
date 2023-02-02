1e92859b649a4276313ae0527441640b989e871d
madeiramcqueen <madeira@mcqueen.net>
2023-02-02 14:01:44 -0800

    Hardcode google maps embed API

--- hiketracker/settings.py
+++ hiketracker/settings.py
@@ -23,9 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-xfkoymuf9lbeq&8_fb7y5etoa4yxhpgf5uj=w6n*4oi#hxb-i^',
-GOOGLE_MAPS_API_KEY = 'AIzaSyAR3xiugHdIMo4eCDHkn0cdn8DS7oY5hfo'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
